ffa6805b919d7618f8511a7be8a0fc0416d76dfa
Darrel Schreyer <dwschreyer@gmail.com>
2017-04-15 20:05:54 +1200

    Updated with some suggestions by jtkech.

---
+++ ExMClTagHelpers/MyExMClH1TagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+
+namespace ClTagHelpers
+{
+    [HtmlTargetElement("my-ex-m-cl-h1")]
+    [OutputElementHint("h1")]
+    public class MyExMClH1TagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "h1";
+            var htmlAttributes = new Dictionary<string, object>();
+            htmlAttributes.Add("style", "background-color: blue; color: white;");
+            var tagBuilder = new TagBuilder(output.TagName);
+            tagBuilder.MergeAttributes(htmlAttributes);
+            output.MergeAttributes(tagBuilder);
+        }
+    }
+}
