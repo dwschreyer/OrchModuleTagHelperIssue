995d0a16e0537d79a32817fba1aaf575880fc06b
Darrel Schreyer <dwschreyer@gmail.com>
2017-04-15 14:25:46 +1200

    Created projects

---
+++ Module1/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Modules;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
+namespace Module1
+{
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            
+        }
+
+        public override void Configure(IApplicationBuilder app, IRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+            routes.MapAreaRoute(
+                "Module1Home",
+                "Module1",
+                "",
+                new { controller = "Home", action = "Index" }
+                );
+        }
+    }
+}
---
+++ Module1/TagHelpers/MyMH1TagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+
+namespace Module1.TagHelpers
+{
+    [HtmlTargetElement("my-m-h1")]
+    [OutputElementHint("h1")]
+    public class MyMH1TagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "h1";
+            var htmlAttributes = new Dictionary<string, object>();
+            htmlAttributes.Add("style", "background-color: blue; color: white;");
+            var tagBuilder = new TagBuilder(output.TagName);
+            tagBuilder.MergeAttributes(htmlAttributes);
+            output.MergeAttributes(tagBuilder);
+        }
+    }
+}
